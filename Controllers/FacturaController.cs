b721b4ebb5cd08fc6a24daa0d0039d0a69994f27
Yeser Sabillon <yeser07@gmail.com>
2026-02-01 20:31:59 -0600

    Creando el header de la factura

---
+++ Controllers/FacturaController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YeserSabillon_PruebaTecnica.Repositorios;
+using YeserSabillon_PruebaTecnica.Models;
+
+namespace YeserSabillon_PruebaTecnica.Controllers
+{
+    public class FacturaController : Controller
+    {
+        private readonly FacturaHeaderRepositorio _facturaHeaderRepositorio;
+        private readonly FacturaDetalleRepositorio _facturaDetalleRepositorio;
+        private readonly ClienteRepositorio _clienteRepositorio;
+        private readonly ProductoRepositorio _productoRepositorio;
+        public FacturaController
+            (
+            FacturaHeaderRepositorio facturaHeaderRepositorio, 
+            FacturaDetalleRepositorio facturaDetalleRepositorio,
+            ClienteRepositorio clienteRepositorio,
+            ProductoRepositorio productoRepositorio
+            )
+        {
+            _facturaHeaderRepositorio = facturaHeaderRepositorio;
+            _facturaDetalleRepositorio = facturaDetalleRepositorio;
+            _clienteRepositorio = clienteRepositorio;
+            _productoRepositorio = productoRepositorio;
+
+
+        }
+
+
+        public IActionResult FacturaView()
+        {
+            List<Cliente> ListaClientes = _clienteRepositorio.ListarClientes();
+
+            return View("~/Pages/Factura/Factura.cshtml",ListaClientes);
+        }
+    }
+}
